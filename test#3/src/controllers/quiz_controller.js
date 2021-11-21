3ee7e9a2118ab29700be7a79163dfeb626275c24
Pierre Penhard <p.penhard@gmail.com>
2021-11-21 15:14:35 +0100

    add several features to the quiz

--- test#3/src/controllers/quiz_controller.js
+++ test#3/src/controllers/quiz_controller.js
@@ -1,4 +1,36 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  
+  static targets = ['panel', 'answer']
+  static values = { index: Number }
+
+  initialize() {
+    this.showCurrentPanel();
+  }
+  
+  select(event) {
+    event.currentTarget.classList.toggle('selected');
+    event.currentTarget.children[0].classList.toggle('selected');
+    const answerIndex = parseInt(event.currentTarget.dataset.answerIndex);
+    this.answerTargets[answerIndex].classList.toggle('d-none');
+  }
+
+  next() {
+    this.indexValue++;
+  }
+
+  previous() {
+    this.indexValue--;
+  }
+
+  indexValueChanged() {
+    this.showCurrentPanel();
+  }
+
+  showCurrentPanel() {
+    this.panelTargets.forEach((element, index) => {
+      element.hidden = index != this.indexValue
+    });
+  }
 }
